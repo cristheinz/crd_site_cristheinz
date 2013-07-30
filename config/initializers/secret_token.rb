c03e8190123178bf4c0e2249b57888f00267fbda
Christiano Domingues <cristheinz@gmail.com>
2013-07-30 14:45:38 +0100

    first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrdSiteCristheinz::Application.config.secret_token = 'bfa944d55b592fb1f059b9e48c2b839de7aee6361046dfcda560875ec4929972fe7228a6c6bbefe19cc3a60ad1217db2dae3b523ed5d1570bf2c2d9eb49d32dd'
